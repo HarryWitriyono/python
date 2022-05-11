fc1eeb84311d2842176376d02747bb098fa0cb40
Harry Witriyono <49424961+HarryWitriyono@users.noreply.github.com>
2022-05-11 21:53:52 +0700

    Update hitungluas.py

--- hitungluas.py
+++ hitungluas.py
@@ -2,8 +2,10 @@ import streamlit as st
 st.sidebar.markdown("### My first Awesome App")
 st.sidebar.markdown("Welcome to my first awesome app. This app is built using Streamlit and uses data source from redfin housing market data. I hope you enjoy!")
 st.title("Hitung Luas Segi Empat")
+st.markdown('''
 print("Hitung Luas Segi Empat")
 panjang=float(input("Input nilai panjangnya : "))
 lebar=float(input("Input nilai lebarnya : "))
 luas=panjang*lebar
 print("Luas segitiga = ",luas," satuan")
+''')
