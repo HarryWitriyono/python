b92efa2921a7d482df6d09e68fa16e364b757163
Harry Witriyono <49424961+HarryWitriyono@users.noreply.github.com>
2022-05-11 21:33:18 +0700

    Create hitungluas.py

---
+++ hitungluas.py
@@ -0,0 +1,5 @@
+print("Hitung Luas Segi Empat")
+panjang=float(input("Input nilai panjangnya : "))
+lebar=float(input("Input nilai lebarnya : "))
+luas=panjang*lebar
+print("Luas segitiga = ",luas," satuan")
