ce27bec71ddb80f03ed5f7ad5389a806558ea927
Harry Witriyono <49424961+HarryWitriyono@users.noreply.github.com>
2022-05-16 10:37:31 +0700

    Update hitungluas.py

--- hitungluas.py
+++ hitungluas.py
@@ -3,11 +3,11 @@ st.sidebar.markdown("### Hitung Luas Segi Empat")
 st.sidebar.markdown("Berikut ini adalah pemrograman sederhana dengan python yang dapat anda coba.  Program ini untuk menghitung luas segi empat.")
 st.title("Hitung Luas Segi Empat")
 st.markdown('''
-print("Hitung Luas Segi Empat")\n
-panjang=float(input("Input nilai panjangnya : "))\n
-lebar=float(input("Input nilai lebarnya : "))\n
+st.title("Hitung Luas Segi Empat")\n
+panjang=st.number_input('Inputkan nilai panjang (0-50)', 0.0, 50.0,0.0,1.0)\n
+lebar=st.number_input('Inputkan nilai lebar (0-50)', 0.0, 50.0,0.0,1.0)\n
 luas=panjang*lebar\n
-print("Luas segitiga = ",luas," satuan")\n
+st.write("Luas segi empat = ",luas," satuan.")\n
 ''')
 panjang=st.number_input('Inputkan nilai panjang (0-50)', 0.0, 50.0,0.0,1.0)
 lebar=st.number_input('Inputkan nilai lebar (0-50)', 0.0, 50.0,0.0,1.0)
